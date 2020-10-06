4187b9ab6087a5aa7d7dff206d24d3102cff4741
Tim <timothy.mcc@comcast.net>
2020-10-05 21:55:07 -0400

    accounted for kinect coordinate rotation and translation

--- matrix.py
+++ matrix.py
@@ -4,10 +4,18 @@ import math
 # takes in skeleton (which has a position)
 # returns the two rotational values of both servos
 def calcRotation(coords):
-    x = coords[0]
-    y = coords[1]
-    z = coords[2]
-    dist = math.sqrt(x**2 + y**2 + z**2)
+    # coords from kinect
+    kinect_x = coords[0]
+    kinect_y = coords[1]
+    kinect_z = coords[2]
+    # kinect rotation angle, translation dist
+    p = 0 # determine
+    trans = 0 # determine
+
+    # coords for inverse kinematics
+    x = kinect_x
+    y = kinect_y*math.cos(p) - kinect_z*math.sin(p) - trans
+    z = kinect_y*math.sin(p) + kinect_z*math.cos(p)
 
     pitch = math.atan2(x,math.sqrt(x**2+z**2))
     yaw = math.atan2(y,z)
